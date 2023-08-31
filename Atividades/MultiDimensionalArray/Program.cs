6c03c7831333a1913ba54cb540634a3a28c449e0
Gabriel de Oliveira <419150@unoesc.lan>
2023-08-30 21:58:04 -0300

    Primeiro Commit

---
+++ Atividades/MultiDimensionalArray/Program.cs
@@ -0,0 +1,19 @@
+﻿int[,] biNumbers = new int[5,2];
+
+int[,,] triNumbers = new int[5,4,3];
+
+int[,] loadedBiNumber = new int[,]  
+{
+    {  9,  5, 15},
+    { 10,-11, 15},
+    {101, 45,  8}
+};
+    
+for(int i = 0; i <loadedBiNumber.GetLength(0); i ++)
+{
+    for(int j = 0; j < loadedBiNumber.GetLength(1); j++)
+    {
+        Console.Write($"[{loadedBiNumber[i,j]}]");
+    }
+    Console.WriteLine("");
+}
